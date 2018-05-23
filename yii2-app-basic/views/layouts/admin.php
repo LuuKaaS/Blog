1ae42c3ded707273a4c09a434cafbc9110279a5c
Lukas Vanek <info.lukas.vanek@gmail.com>
2018-05-23 16:19:52 +0100

    update admin layout

--- yii2-app-basic/views/layouts/admin.php
+++ yii2-app-basic/views/layouts/admin.php
@@ -38,23 +38,11 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Home', 'url' => ['/admin/default/index']],
             ['label' => 'Articles', 'url' => ['/admin/article/index']],
             ['label' => 'Category', 'url' => ['/admin/category/index']],
-            ['label' => 'Tag', 'url' => ['/admin/tag/index']],
-            
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+            ['label' => 'Tag', 'url' => ['/admin/tag/index']]
+           
         ],
     ]);
     NavBar::end();
